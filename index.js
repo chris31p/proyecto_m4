19ade35cd700a996fe47bf7aba885cdf65924e12
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2024-11-24 17:33:23 -0500

    Update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const app = require('./src/app.js');
 
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API Reservas. Visita /api-docs para la documentación.');
+  });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
     console.log(`Servidor corriendo en el puerto ${PORT}`)
