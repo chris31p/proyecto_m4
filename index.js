bcb1c4cb2d221b48ad49fecf0e330bea896889a3
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2024-11-24 18:13:02 -0500

    Fix: msg bienvenida API

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 require('dotenv').config();
 const app = require('./src/app.js');
 
-app.get('/', (req, res) => {
-    res.send('Bienvenido a la API Reservas. Visita /api-docs para la documentación.');
-  });
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>{
     console.log(`Servidor corriendo en el puerto ${PORT}`)
--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ const specs = require('../swagger/swagger.js');
 app.use(express.json()); //Permite que express maneje el formato json en el body de las solicitudes
 //Configuración del endpoint de Swagger UI
 app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(specs));
-//Definit la base de rutas de la API
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API Reservas. Visita /api-docs para la documentación.');
+  });
+//Definir la base de rutas de la API
 app.use('/', (req, res, next) => {
     console.log('Solicitud recibida en /api/reservas'); // Depuración
     next(); // Continua al siguiente middleware
