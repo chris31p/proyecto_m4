fb8ce82e785de0eae52a8bdfa4db0e6a0ac38d4a
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2024-11-24 18:20:28 -0500

    Update

--- src/app.js
+++ src/app.js
@@ -5,11 +5,12 @@ const swaggerUI = require('swagger-ui-express');
 const specs = require('../swagger/swagger.js');
 
 app.use(express.json()); //Permite que express maneje el formato json en el body de las solicitudes
-//Configuración del endpoint de Swagger UI
-app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(specs));
 app.get('/', (req, res) => {
     res.send('Bienvenido a la API Reservas. Visita /api-docs para la documentación.');
   });
+//Configuración del endpoint de Swagger UI
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(specs));
+
 //Definir la base de rutas de la API
 app.use('/', (req, res, next) => {
     console.log('Solicitud recibida en /api/reservas'); // Depuración
