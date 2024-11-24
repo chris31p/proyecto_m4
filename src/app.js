8e067df424c7b607071c86d8132ddc16ee681572
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2024-11-24 18:58:14 -0500

    Fix: Mensaje de Bienvenida en Render app.js

--- src/app.js
+++ src/app.js
@@ -5,11 +5,17 @@ const swaggerUI = require('swagger-ui-express');
 const specs = require('../swagger/swagger.js');
 
 app.use(express.json()); //Permite que express maneje el formato json en el body de las solicitudes
+
+// Ruta raíz para el mensaje de bienvenida
+app.get('/', (req, res) => {
+    res.send('BIENVENIDO A LA API DE RESERVAS. Visita /api-docs para la documentación.');
+});
+
 //Configuración del endpoint de Swagger UI
 app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(specs));
 
 //Definir la base de rutas de la API
-app.use('/', (req, res, next) => {
+app.use('/api/reservas', (req, res, next) => {
     console.log('Solicitud recibida en /api/reservas'); // Depuración
     next(); // Continua al siguiente middleware
 },reservasRoutes); //Definimos la ruta base para las reservas
